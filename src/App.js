1d64cfbcb181acfc6601469b23e32c9168d452c7
Luke Prescott <luke.prescott@presport.us>
2020-06-20 15:15:57 -0400

    chore: added material ui, react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import './App.scss'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const lightTheme = createMuiTheme({
+	overrides: {}
+})
+
+const darkTheme = createMuiTheme({
+	overrides: {}
+})
 
 function App() {
+	const [darkMode, setDarkMode] = React.useState(false)
+
 	return (
-		<div className="App">
+		<ThemeProvider
+			className="App"
+			theme={darkMode ? darkTheme : lightTheme}
+		>
 			<header className="App-header">Header</header>
 			<div className="App-body">Home</div>
-		</div>
+		</ThemeProvider>
 	)
 }
 
