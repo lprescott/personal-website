a17552be213946402a6c39375e374a38942ee2f5
Luke Prescott <luke.prescott@presport.us>
2020-06-20 15:35:42 -0400

    feat: dark mode init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.scss'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
-
-const lightTheme = createMuiTheme({
-	overrides: {}
-})
-
-const darkTheme = createMuiTheme({
-	overrides: {}
-})
+import {
+	orange,
+	lightBlue,
+	deepPurple,
+	deepOrange
+} from '@material-ui/core/colors'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 function App() {
-	const [darkMode, setDarkMode] = React.useState(false)
+	const [open, setOpen] = useState(true)
+	const [darkState, setDarkState] = useState(false)
+	const palletType = darkState ? 'dark' : 'light'
+	const mainPrimaryColor = darkState ? orange[500] : lightBlue[500]
+	const mainSecondaryColor = darkState ? deepOrange[900] : deepPurple[500]
+
+	const theme = createMuiTheme({
+		palette: {
+			type: palletType,
+			primary: {
+				main: mainPrimaryColor
+			},
+			secondary: {
+				main: mainSecondaryColor
+			}
+		}
+	})
 
 	return (
-		<ThemeProvider
-			className="App"
-			theme={darkMode ? darkTheme : lightTheme}
-		>
-			<header className="App-header">Header</header>
-			<div className="App-body">Home</div>
+		<ThemeProvider theme={theme} className="App">
+			<CssBaseline />
+			Hello World
 		</ThemeProvider>
 	)
 }
