d5f7d0ccd12f0b5010ddccd891d11100c252ed57
Luke Prescott <luke.prescott@presport.us>
2020-06-20 22:20:47 -0400

    feat: animated chevron

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Frame } from 'framer'
+import { useCycle } from 'framer-motion'
 
 const Content = () => {
-	return <div>Content</div>
+	const [twist, cycle] = useCycle({ scaleX: 1 }, { scaleX: -1 })
+
+	return (
+		<div>
+			<Frame animate={twist} onTap={() => cycle()}>
+				Test
+			</Frame>
+		</div>
+	)
 }
 
 export default Content
--- src/SideDrawer/SideDrawer.js
+++ src/SideDrawer/SideDrawer.js
@@ -1,13 +1,15 @@
 import './SideDrawer.scss'
 import { makeStyles } from '@material-ui/core/styles'
-import { Tabs, Tab } from '@material-ui/core'
+import { Tabs, Tab, IconButton } from '@material-ui/core'
 import AccountTreeRoundedIcon from '@material-ui/icons/AccountTreeRounded'
 import DescriptionRoundedIcon from '@material-ui/icons/DescriptionRounded'
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded'
-import OpenDrawer from './OpenDrawer'
 import PersonIcon from '@material-ui/icons/Person'
 import PropTypes from 'prop-types'
 import React from 'react'
+import { Frame } from 'framer'
+import { useCycle } from 'framer-motion'
+import ArrowForwardIosRoundedIcon from '@material-ui/icons/ArrowForwardIosRounded'
 
 const useStyles = makeStyles({
 	root: {
@@ -15,13 +17,22 @@ const useStyles = makeStyles({
 	}
 })
 
+const useStyles2 = makeStyles({
+	root: {
+		color: 'white'
+	}
+})
+
 const SideDrawer = (props) => {
+	const [flip, cycle] = useCycle({ scaleX: 1, x: 10 }, { scaleX: -1, x: 100 })
+
 	const [value, setValue] = React.useState(0)
 	const handleChange = (event, newValue) => {
 		setValue(newValue)
 	}
 
 	const classes = useStyles(props)
+	const classes2 = useStyles2()
 
 	return (
 		<Tabs
@@ -43,7 +54,21 @@ const SideDrawer = (props) => {
 			/>
 			<Tab icon={<PersonIcon fontSize="large" />} label="contact" />
 
-			<OpenDrawer open={props.open} setOpen={props.setOpen} />
+			<Frame
+				animate={flip}
+				onTap={() => {
+					cycle()
+					props.setOpen(!props.open)
+				}}
+				size={50}
+				background={null}
+				bottom={5}
+				transition={{ duration: 0.1 }}
+			>
+				<IconButton>
+					<ArrowForwardIosRoundedIcon classes={classes2} />
+				</IconButton>
+			</Frame>
 		</Tabs>
 	)
 }
