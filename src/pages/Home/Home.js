8c6f4a9233a889927c1c083ff0cd51a5b377d901
Luke Prescott <luke.prescott@presport.us>
2020-10-20 17:24:23 -0400

    chore: add three

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,20 +3,8 @@ import PageInner from '../../PageInner'
 
 const Home = () => {
 	return (
-		<PageInner className="HomePage">
-			<div>
-				<span>
-					Hello, my name is Luke Prescott. I am a full-stack web
-					developer and mathematics enthusiast. Please feel free to
-					check out my Github repositories and play around with the
-					demos on this site. Contact me here
-				</span>
-				<a href="mailto:luke.prescott@presport.us">
-					luke.prescott@presport.us
-				</a>
-				_
-			</div>
-			<div>This will be my terminal</div>
+		<PageInner>
+			Home Page
 		</PageInner>
 	)
 }
