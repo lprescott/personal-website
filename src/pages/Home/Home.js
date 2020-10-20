6d40cdfd2c1527d7a8513c5eaa7f5b9ef704f0b5
Luke Prescott <luke.prescott@presport.us>
2020-10-20 18:21:41 -0400

    feat: use of p5

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import PageInner from '../../PageInner'
+import Sketch from 'react-p5'
 
 const Home = () => {
+
+    let x = 50
+    const y = 50
+ 
+    const setup = (p5, canvasParentRef) => {
+        // use parent to render the canvas in this ref
+        // (without that p5 will render the canvas outside of your component)
+        p5.createCanvas(500, 500).parent(canvasParentRef)
+    }
+ 
+    const draw = (p5) => {
+        p5.background(255)
+        p5.ellipse(x, y, 70, 70)
+        // NOTE: Do not use setState in the draw function or in functions that are executed
+        // in the draw function...
+        // please use normal variables or class properties for these purposes
+        x++
+	}
+	
 	return (
 		<PageInner>
-			Home Page
+			<Sketch setup={setup} draw={draw} />
 		</PageInner>
 	)
 }
